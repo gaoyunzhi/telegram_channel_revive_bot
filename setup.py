d1b3980fd3295526bc0c00366276ce1bc25cbb05
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-11-27 11:40:02 -0800

    improve setup

--- setup.py
+++ setup.py
@@ -1,36 +1,29 @@
 import os
 import sys
-import json
 
-REQUIRED_KEYS = set(['bot_token'])
+def kill():
+	os.system("ps aux | grep ython | grep 'revive.py' | awk '{print $2}' | xargs kill -9")
 
 def setup(arg = ''):
+	if arg == 'kill':
+		return kill()
+
 	RUN_COMMAND = 'nohup python3 -u revive.py &'
 
-	CREDENTIALS = {}
+	if arg != 'debug':
+		r = os.system('sudo pip3 install -r requirements.txt')
+		if r != 0:
+			os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
+			os.system('sudo python3 get-pip.py')
+			os.system('rm get-pip.py')
+			os.system('sudo pip3 install -r requirements.txt')
 	try:
-		with open('CREDENTIALS') as f:
-			CREDENTIALS = json.load(f)
-	except Exception as e:
-		print(e)
-
-	for key in REQUIRED_KEYS:
-		if key not in CREDENTIALS:
-			print('ERROR: please fill the CREDENTIALS file in json format, required keys : ' + ', '.join(sorted(REQUIRED_KEYS)))
-			return
-
-	if arg != 'reload' and arg != 'debug':
-		os.system('curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py')
-		os.system('sudo python3 get-pip.py')
-		os.system('rm get-pip.py')
-
-		os.system('sudo pip3 install -r requirements.txt')
+		from telegram.ext import Updater, MessageHandler, Filters
+	except:
 		os.system('sudo pip3 install python-telegram-bot --upgrade') # need to use some experiement feature, e.g. message filtering
-			
-	# kill the old running bot if any. If you need two same bot running in one machine, use mannual command instead
-	os.system("ps aux | grep ython | grep revive | awk '{print $2}' | xargs kill -9")
 
-	if arg == 'debug':
+	kill()
+	if arg.startswith('debug'):
 		os.system(RUN_COMMAND[6:-2])
 	else:
 		os.system(RUN_COMMAND)
